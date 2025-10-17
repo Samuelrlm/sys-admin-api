785bd3b8881b63326874ec462ab46889e9482c05
Samuel Lima <105258182+Samuelrlm@users.noreply.github.com>
2025-10-17 09:57:49 -0300

    init

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+const productsRoutes = require("./src/routes/products")
+
+app.use(express.json())
+
+const PORT = 4467;
+
+app.get("/", (req, res) => {
+    res.send("Olá Samuel, seja bem vindo!")
+})
+
+app.use(productsRoutes)
+
+app.listen(PORT, () => {
+    console.log(`Servidor rodando na porta ${PORT}`)
+})
